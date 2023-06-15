8c37918970aa1818b8bf2f7023167f88905ab160
Mohamed Elsherbiny <mohamedelsherbiny212@gmail.com>
2023-06-15 20:37:03 +0300

    Creating an variable explaination

--- main.c
+++ main.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 
+/*in this example i will make the user
+ the one who add the number and to figure out
+  the deference between scanf / printf */
+
 int main()
 {
-    printf("*     *\n");
-    printf(" *   *\n");
-    printf("  * *\n");
-    printf("   *\n");
-    printf("  * *\n");
-    printf(" *   *\n");
-    printf("*     *\n");
-
+    int num1; // developer input
+    num1 = 20;
+    printf("num1 is %d \n", num1);
+    int num2; // user input so you should add the num in the terminal
+    scanf("%d", &num2);
+    printf("num2 is %d \n", num2);
     return 0;
 }
