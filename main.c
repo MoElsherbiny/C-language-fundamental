7afd109471335a389d1f30fcafbbc33419753369
Mohamed Elsherbiny <mohamedelsherbiny212@gmail.com>
2023-07-26 18:42:26 +0300

    Keywords

--- main.c
+++ main.c
@@ -2,6 +2,13 @@
 
 int main()
 {
-    printf("Hello World\n");
+    double number = 12.45;
+    float number_1 = 12.9f;
+    char character = 'z';
+    char name;
+    printf("The double number is %.2lf\n", number);
+    printf("The float number is %.1f\n", number_1);
+    printf("%c\n", character);
+    printf("The size of char = %zu\n", sizeof(name));
     return 0;
 }
