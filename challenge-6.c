85071b3484af7448bfd5ecef7c9d7c6e82d3af2c
Mohamed Elsherbiny <mohamedelsherbiny212@gmail.com>
2023-07-26 22:51:16 +0300

    ind quotient and remainder of Two Integers

---
+++ challenge-6.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * C program to find quotient and remainder of Two Integers
+ */
+
+int main() {
+    int dividend, divisor, quotient, remainder;
+    printf("Enter dividend: ");
+    scanf("%d", &dividend);
+    printf("Enter divisor: ");
+    scanf("%d", &divisor);
+
+    // Computes quotient
+    quotient = dividend / divisor;
+
+    // Computes remainder
+    remainder = dividend % divisor;
+
+    printf("Quotient = %d\n", quotient);
+    printf("Remainder = %d", remainder);
+    return 0;
+}
