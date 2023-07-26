43c3e14272ec65a27779d7f3277dbd8fde99034f
Mohamed Elsherbiny <mohamedelsherbiny212@gmail.com>
2023-07-26 21:17:33 +0300

    Print Hello - World to the screen

---
+++ challenge-1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * Write a Program that print a sentence 
+ * 
+*/
+
+/**
+ * main - prints Hello World
+ * Return: 0 on success
+ */
+int main(void)
+{
+    printf("Hello World\n");
+    return (0);
+}
