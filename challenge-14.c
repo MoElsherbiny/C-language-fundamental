dd792b0efbaff005538922a44a57ee78de90acfb
Mohamed Elsherbiny <mohamedelsherbiny212@gmail.com>
2023-07-27 17:48:24 +0300

    While-loop

---
+++ challenge-14.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * Write a Program that prints from 1 to 5
+*/
+
+int main()
+{
+    int i = 1;
+    while(i <=5)
+    {
+        printf("%d\n", i);
+        i++;
+    }
+    return 0;
+}
