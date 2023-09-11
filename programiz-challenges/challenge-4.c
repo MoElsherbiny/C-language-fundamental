c7dd043c0c4fce63f93e824c5b07b1ee921b9974
Mohamed Elsherbiny <mohamedelsherbiny212@gmail.com>
2023-09-11 04:43:23 +0300

    function with No Argument Passed and No Return Value

---
+++ programiz-challenges/challenge-4.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * Write a function with No Argument Passed and No Return Value 
+ * This function Check if the number is Prime or not 
+*/
+void checkPrimeNumber();
+
+int main()
+{
+    checkPrimeNumber(); // argument is not passed
+    return 0;
+}
+
+// return type is void meaning doesn't return any value
+void checkPrimeNumber()
+{
+    int n, i, flag = 0;
+
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+
+    // 0 and 1 are not prime numbers
+    if (n == 0 || n == 1)
+        flag = 1;
+
+    for (i = 2; i <= n / 2; ++i)
+    {
+        if (n % i == 0)
+        {
+            flag = 1;
+            break;
+        }
+    }
+
+    if (flag == 1)
+        printf("%d is not a prime number.", n);
+    else
+        printf("%d is a prime number.", n);
+}
