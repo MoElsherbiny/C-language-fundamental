073bc343b30e71e5cb1b5e9319025e8181326750
Mohamed Elsherbiny <mohamedelsherbiny212@gmail.com>
2023-09-11 04:06:00 +0300

    Square root Function [Standard Library]

---
+++ programiz-challenges/challenge-3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * Write a square root standard library function 
+ */
+/**
+ * First Of All we should include <math.h> library
+ * Second use sqrt() function 
+*/
+int main ()
+{
+    float number = sqrt(25);
+    printf("%f \n", number);
+    return (0);
+}
